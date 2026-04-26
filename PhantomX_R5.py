dc18fbe709ebec37c075dab148260ec8d1573110
Shawn <yushawnong@gmail.com>
2026-04-26 13:01:17 +0800

    changer5

--- PhantomX_R5.py
+++ PhantomX_R5.py
@@ -6,7 +6,7 @@ from ACL_FLL_v04_test import *
 ################## Shared and local constants ##################
 
 # Adapter configuration: (LeftPower, RightPower, LeftLimit, RightLimit)
-ROUTE_ADAPTER_POWER = (0, 0, 30, 30)
+ROUTE_ADAPTER_POWER = (-35, -35, 30, 30)
 
 # Route-Specific PID Gains
 STR_KP_CUSTOM = 1.5
@@ -33,7 +33,25 @@ def Route5(laura: Laura):
     laura.hub_status_light(Color.MAGENTA)
 
     """ Start your code here """
-    laura.wall_square()
+    laura.wall_square(power=35)
+    laura.gyro_acc(power= -80 , distance= 550)
+    laura.gyro_point_turn(46)
+    laura.gyro_acc(power= -50 , distance= 115 , angle= 45)
+    laura.gyro_point_turn(angle= -46)
+    laura.gyro_acc(power= -50 , distance= 150 , angle= -45 , stop= False)
+    laura.gyro_time(power= -35 , duration= 1200 , angle= -45)
+    laura.adapter_motor_seconds( port= LEFT_ADAPTER , speed= 1000 , duration= 1000)
+    laura.gyro_acc( power= 50 , distance= 150 , angle= -45)
+    laura.gyro_lock_turn( port= LEFT_DRIVE , angle= 0)
+    laura.gyro_acc( power= 60 , distance= 40)
+    laura.adapter_motor_seconds( port=RIGHT_ADAPTER , speed=1000 , duration= 1200, wait_complete= False)
+    laura.gyro_point_turn(angle= 90)
+    laura.gyro_time(power= 40 , duration= 1200 , angle= 88)
+    laura.adapter_motor_seconds( port=RIGHT_ADAPTER , speed= -1000 , duration= 1200 )
+    laura.adapter_motor_seconds( port=RIGHT_ADAPTER , speed= 1000 , duration= 1200 )
+    laura.gyro_acc( power= -60 , distance= 100 , angle= 90)
+    laura.gyro_point_turn(angle= 200)
+    laura.gyro_acc( power= -70 , distance= 600 , angle= 200)
     
 
 
