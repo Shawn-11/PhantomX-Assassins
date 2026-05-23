09e04c4e3422499f99903ad488bf69300fb2f1ed
Shawn <yushawnong@gmail.com>
2026-05-23 21:07:51 +0800

    r9 capture (no release)

--- PhantomX_R9.py
+++ PhantomX_R9.py
@@ -6,7 +6,7 @@ from ACL_FLL_v04_test import *
 ################## Shared and local constants ##################
 
 # Adapter configuration: (LeftPower, RightPower, LeftLimit, RightLimit)
-ROUTE_ADAPTER_POWER = (-40, -40, 30, 30)
+ROUTE_ADAPTER_POWER = (40, -40, 30, 30)
 
 # Route-Specific PID Gains
 STR_KP_CUSTOM = 1.5
@@ -39,24 +39,15 @@ def Route9(laura: Laura):
     laura.line_follow_degree(direction= -1 , power= 50 ,port= RIGHT_COLOUR ,degree= 80 , stop= False )
     laura.line_follow_detect_reflected(direction= -1 , power= 50 ,port= RIGHT_COLOUR , detect_port= LEFT_COLOUR , threshold= 30)
     laura.encoder_degree(left_power= -70 , right_power= 70 , degree= 140 , stop= False)   
-    laura.gyro_acc(power= 80 , distance= 500 , angle= -64)
-    laura.gyro_lock_turn(port= LEFT_DRIVE , angle= -10)
-    laura.adapter_motor_seconds(port= RIGHT_ADAPTER , speed= 500 , duration= 4000 , wait_complete= False)
-    laura.gyro_point_turn(angle= -89 , stop= False)
-    laura.gyro_acc(power= 80 , distance= 100 , angle= -90 , stop= False)
-    laura.gyro_time(power= 50 , duration= 800 , angle= -90)
-    # wait(500)
-    laura.adapter_motor_seconds(port= LEFT_ADAPTER , speed= -1000 , duration= 1200 , wait_complete= False)
-    laura.gyro_acc(power= -60 , distance= 200 , angle= -90)
-    laura.gyro_point_turn(angle= -230)
-    laura.gyro_acc(power= 80 , distance= 240 , angle= -230)
-    laura.adapter_motor_seconds(port= RIGHT_ADAPTER , speed= -800 , duration= 1200 ,wait_complete=False)
-    laura.gyro_lock_turn(port= LEFT_DRIVE , angle= -210)
-    wait(200)
-
-
-    # laura.encoder_degree(left_power= -70 , right_power= 70 , degree= 195 )
-
+    laura.gyro_acc(power= 80 , distance= 480 , angle= -64)
+    laura.gyro_lock_turn(LEFT_DRIVE, 0)
+    laura.gyro_lock_turn(RIGHT_DRIVE, -50)
+    laura.gyro_lock_turn(LEFT_DRIVE, -90)
+    laura.gyro_degree(60, 230, -90, False)
+    laura.gyro_degree(60, 50, -90, True)
+    laura.adapter_motor_seconds(port= LEFT_ADAPTER , speed= -1000 , duration= 1200 , wait_complete=False)
+    laura.adapter_motor_seconds(RIGHT_ADAPTER, 1000, 4400, wait_complete=True)
+    
 
 
     """ Route end """
